90cf8ca30a18d5c556fe87425bb8170d9bca91c1
vikrantsingla26 <vikrantsingla5@gmail.com>
2026-07-13 15:27:24 +0530

    add login api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-
+const {validateSignUpData} = require("./utils/validation")
 const connectdb = require("./config/database")
 const User = require("./models/user");
-
+const bcrypt = require("bcrypt")
 
 const app = express();
 app.use(express.json())
 app.post("/signup", async (req, res) => {
 
     try {
-        const user = new User(req.body);
+        validateSignUpData(req);
+        const {password,firstName,lastName,emailId,age} = req.body;
+        const passwordhash = await bcrypt.hash(password,10);
+        const user  = new User({
+            firstName,
+            lastName,
+            age:18,
+            password :passwordhash,
+            emailId
+        })
+        
+
         await user.save();
         res.send("data succefully save ");
     }
@@ -19,6 +30,27 @@ app.post("/signup", async (req, res) => {
     }
 
 })
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId });
+
+        if (!user) {
+            return res.status(400).send("Invalid email or password");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).send("Invalid email or password");
+        }
+
+        res.send("Login successfully");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 app.get("/user", async (req, res) => {
     try {
         const email = req.body.emailId;
